b8b5e30e09ee4aa3cf11697733e47aad8fbb4d26
cool0505 <joowonkoh@naver.com>
2023-01-03 23:08:55 +0900

    solution

--- 8958.py
+++ 8958.py
@@ -1,8 +1,13 @@
 a=int(input())
 
 for i in range(a):
+    score=0
+    all=0
     str=input()
     for i in str:
-        if(i=="o"):
-            
-
+        if(i=="O"):
+            score=score+1
+        else:
+            score=0
+        all=all+score
+    print(all)
