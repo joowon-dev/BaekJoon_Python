d27f619271b41122040b8bc89577cb649f350259
joowonkoh@naver.com <joowonkoh@naver.com>
2023-07-24 16:40:25 +0900

    sol 24883.2558 2741

---
+++ 24883.py
@@ -0,0 +1,5 @@
+N=input()
+if(N=='N'or N=='n'):
+    print("Naver D2")
+else:
+    print("Naver Whale")
---
+++ 2741.py
@@ -0,0 +1,5 @@
+N= int(input())
+for i in range(1,100001):
+    print(i)
+    if(i==N):
+        break
