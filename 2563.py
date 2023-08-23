57671c9f3f252746ea0e8999059f07ab40aa296e
joowonkoh@naver.com <joowonkoh@naver.com>
2023-08-23 14:41:14 +0900

    sol 2563 10757 10815

--- 2563.py
+++ 2563.py
@@ -1,19 +1,14 @@
 A=int(input())
-arr=[[]*100]*100
+arr=[[0 for i in range(100)] for i in range(100)]
 temp=0
 for i in range(A):
   B, C=map(int,input().split())
-  for i in range(len(arr)):
-    X=0
-    Y=0
-    if (arr[i][0]<B and arr[i][0]+10>B):
-      X=20-(B+10-arr[i][0])
-    if (arr[i][0]>B and arr[i][0]<B+10):
-      X=20-(arr[i][0]+10-B)
-    if (arr[i][1]<C and arr[i][1]+10>C):
-      Y=20-(C+10-arr[i][1])
-    if (arr[i][1]>C and arr[i][1]<C+10):
-      Y=20-(arr[i][1]+10-C)
-    temp=X*Y+temp
-  arr.append([B,C])
-print(100*A-temp)
+  for x in range(10):
+    for y in range(10):
+      arr[x+B][y+C]=1
+count=0
+for x in range(100):
+  for y in range(100):
+    if (arr[x][y]==1):
+      count+=1
+print(count)
