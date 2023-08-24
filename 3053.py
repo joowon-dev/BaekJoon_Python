86141093dfee750e292cff57d18168db3ac1be33
joowonkoh@naver.com <joowonkoh@naver.com>
2023-08-24 14:04:52 +0900

    sol 11723 3053 4101

---
+++ 3053.py
@@ -0,0 +1,3 @@
+R= int(input())
+print(format(R*R*3.141592653589793,".6f"))
+print(format(R*R*2,".6f"))
---
+++ 4101.py
@@ -0,0 +1,8 @@
+while(1):
+    A,B = map(int,input().split())
+    if(A>B):
+        print("Yes")
+    elif(A==0 and B==0):
+        break
+    else:
+        print("No")
